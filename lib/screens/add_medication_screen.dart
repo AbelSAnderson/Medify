3313ec79f1d1eb5732f2543cf9f27442005a9d63
Hasan Muslemani <w0721766@myscc.ca>
2021-02-28 14:46:38 -0500

    Set Up Project
    * Removed default app
    
    * Added primary/secondary colours
    
    * Created empty homepage screen

---
+++ lib/screens/add_medication_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddMedication extends StatefulWidget {
+  @override
+  _AddMedicationState createState() => _AddMedicationState();
+}
+
+class _AddMedicationState extends State<AddMedication> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(child: null);
+  }
+}
