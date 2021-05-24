19487a9cdd00b92026af66793487699a6aefbe11
Hasan Muslemani <w0721766@myscc.ca>
2021-05-23 23:16:34 -0400

    #50 Update App Icon & Colours
    * Changed App Icon to the new Rx-Medify Icon
    
    * Updated Primary Colour to darker blue to match client's product package & website
    
    * Updated Accent colour from pink to darkish purple to blend better with the new primary colour

--- lib/main.dart
+++ lib/main.dart
@@ -40,16 +40,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //create custom color swatch for flutter to use on material components
     const Map<int, Color> colors = {
-      50: Color.fromRGBO(1, 105, 255, 0.1),
-      100: Color.fromRGBO(1, 105, 255, 0.2),
-      200: Color.fromRGBO(1, 105, 255, 0.3),
-      300: Color.fromRGBO(1, 105, 255, 0.4),
-      400: Color.fromRGBO(1, 105, 255, 0.5),
-      500: Color.fromRGBO(1, 105, 255, 0.6),
-      600: Color.fromRGBO(1, 105, 255, 0.7),
-      700: Color.fromRGBO(1, 105, 255, 0.8),
-      800: Color.fromRGBO(1, 105, 255, 0.9),
-      900: Color.fromRGBO(1, 105, 255, 1),
+      50: Color.fromRGBO(52, 88, 166, 0.1),
+      100: Color.fromRGBO(52, 88, 166, 0.2),
+      200: Color.fromRGBO(52, 88, 166, 0.3),
+      300: Color.fromRGBO(52, 88, 166, 0.4),
+      400: Color.fromRGBO(52, 88, 166, 0.5),
+      500: Color.fromRGBO(52, 88, 166, 0.6),
+      600: Color.fromRGBO(52, 88, 166, 0.7),
+      700: Color.fromRGBO(52, 88, 166, 0.8),
+      800: Color.fromRGBO(52, 88, 166, 0.9),
+      900: Color.fromRGBO(52, 88, 166, 1),
     };
     return MaterialApp(
       builder: (context, child) {
@@ -84,9 +84,9 @@ class MyApp extends StatelessWidget {
       title: 'Medify',
       theme: ThemeData(
         brightness: Brightness.light,
-        primarySwatch: MaterialColor(0xFF0169FF, colors),
+        primarySwatch: MaterialColor(0xFF3458a6, colors),
         //primaryColor: Color.fromRGBO(1, 105, 255, 1),
-        accentColor: Color.fromRGBO(230, 0, 233, 1),
+        accentColor: Color.fromRGBO(175, 0, 233, 1),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: BlocProvider<LoginCubit>(
